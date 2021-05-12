0ec0e4f3dcdc5590800c25cc9d9d9121caab6c09
archyrysss <archyrysss@gmail.com>
2021-05-12 22:11:53 +0300

    Добавьте файлы проекта.

---
+++ HotNail/WebPages/Presentation.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Presentation.aspx.cs" Inherits="HotNail.WebPages.Presentation" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button runat="server" Height="50" Width="50" BackColor="Tomato" />
+        </div>
+        <div>
+            <asp:Repeater ID="RepeaterImagePresentation" runat="server">
+                <ItemTemplate>
+                    <asp:Image runat="server" ImageUrl='<%# Container.DataItem %>' Height="400" Width="400"/>
+                </ItemTemplate>
+            </asp:Repeater>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ HotNail/WebPages/Presentation.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HotNail.WebPages
+{
+    public partial class Presentation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string[] files = Directory.GetFiles(Server.MapPath("~/Images/Presentation"));
+                List<string> imgs = new List<string>();
+                foreach (string img in files)
+                {
+                    imgs.Add("~/Images/Presentation/" + Path.GetFileName(img));
+                }
+
+                RepeaterImagePresentation.DataSource = imgs;
+                RepeaterImagePresentation.DataBind();
+            }
+        }
+    }
+}
